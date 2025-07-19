895edcca571140f14293a06b8191c638a1829c47
InushK <uvinduinushavp@gmail.com>
2025-07-19 22:56:23 +0530

    Completed customer edit and redirect

--- src/main/java/com/icbt/servlet/CustomerServlet.java
+++ src/main/java/com/icbt/servlet/CustomerServlet.java
@@ -2,7 +2,6 @@ package com.icbt.servlet;
 
 import com.icbt.model.Customer;
 import com.icbt.service.CustomerService;
-import com.icbt.util.DBConnection;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,18 +9,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/CustomerServlet")
-
-
 public class CustomerServlet extends HttpServlet {
     private CustomerService customerService = new CustomerService();
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -53,12 +47,14 @@ public class CustomerServlet extends HttpServlet {
         }
 
         if (success) {
-            response.sendRedirect("main-menu.jsp");
+            // ✅ Redirect to customer list (doGet will handle loading and forwarding)
+            response.sendRedirect("CustomerServlet");
         } else {
             response.sendRedirect("error.jsp");
         }
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
--- src/main/webapp/show-customer.jsp
+++ src/main/webapp/show-customer.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: user
-  Date: 7/19/2025
-  Time: 7:50 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ page import="java.util.*, com.icbt.model.Customer" %>
 
